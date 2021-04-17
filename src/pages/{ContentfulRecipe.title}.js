9322262f3907bcfd27fe47704402c5c92162c847
mozieblo <m.ozieblo@wp.pl>
2021-04-17 09:56:11 +0200

    query with variable

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 const RecipeTemplate = props => {
   console.log(props);
@@ -6,8 +7,19 @@ const RecipeTemplate = props => {
   return (
     <div>
       <h4>{props.params.title}</h4>
+      <p>Title: {props.data.contentfulRecipe.title}</p>
+      <p>Cook time: {props.data.contentfulRecipe.cookTime}</p>
     </div>
   );
 };
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+    }
+  }
+`;
 
 export default RecipeTemplate;
