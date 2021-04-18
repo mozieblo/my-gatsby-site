3791c1a359aec5af7c94db1b1b3dd4fe7f8d68b0
mozieblo <m.ozieblo@wp.pl>
2021-04-18 17:08:26 +0200

    Formspree.io - for form handle - added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -28,7 +28,11 @@ const Contact = ({ data }) => {
             </p>
           </article>
           <article>
-            <form className="form contact-form">
+            <form
+              className="form contact-form"
+              action="https://formspree.io/f/mzbyzrlq"
+              method="POST"
+            >
               <div className="form-row">
                 <label htmlFor="name">your name</label>
                 <input type="text" name="name" id="name" />
